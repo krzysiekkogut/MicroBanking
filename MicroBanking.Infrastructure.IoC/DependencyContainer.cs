206c16da2a5fec44656836eca27a9dc3470d970f
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2020-04-12 20:39:47 +0200

    Create base for IoC

---
+++ MicroBanking.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroBanking.Domain.Core.Bus;
+using MicroBanking.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroBanking.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
